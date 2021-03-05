c2eca8effbb24cad0d7f84c688a2f2d6ebc0a3df
Matias Kamien <mkamien@medallia.com>
2021-03-05 18:16:39 -0300

    Add car models and car prices as constant arrays in RobotBuilder

--- app/models/robot_builder.rb
+++ app/models/robot_builder.rb
@@ -2,15 +2,35 @@ class RobotBuilder
 
   attr_reader :car_factory
 
+  CAR_MODELS = %w[Megane Tida Corsa March Up Gol Polo Fun Golf Vento]
+  CAR_PRICES = [ 10000, 25000, 7500, 12800, 30000, 50000 ]
+
   def initialize
     @car_factory = CarFactory.create!
   end
 
   def create_car
-    car = Car.create! year: 2021, model: "Nissan Tida", price: 150, cost_price: 100
+    price = random_price
+    car = Car.create! year: current_year, model: random_model, price: price, cost_price: calculate_cost_price(price)
     @car_factory.assembly_lines.each { |assembly_line| assembly_line.execute_action_to_car car }
     car.assembly_stage_completed!
     car
+  end
+
+  def current_year
+    Time.new.year
+  end
+
+  def random_model
+    CAR_MODELS[rand(10)]
+  end
+
+  def random_price
+    CAR_PRICES[rand(6)]
+  end
+
+  def calculate_cost_price(price)
+    0.5*price
   end
 
 end
