1638d17b113b18c21f3636b7ff1b270b3f5ebfe8
Matias Kamien <mkamien@medallia.com>
2021-03-06 20:41:41 -0300

    Add the ability to know if the car has any defect to the computer

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,2 +1,7 @@
 class Computer < Part
+
+  def car_has_any_defect
+    self.car.parts.any? { |part| part.has_defect }
+  end
+
 end
--- spec/models/computer_spec.rb
+++ spec/models/computer_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Computer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:car) { Car.new }
+  let(:computer) { Computer.new }
+
+  before(:each) do
+    computer.set_defect NoDefect.new
+    computer.car = car
+    car.parts << computer
+  end
+
+  it "should know if the car has any defect when it effectively has a defect" do
+    seat = Seat.new
+    seat.set_defect ConcreteDefect.new
+    car.parts << seat
+
+    expect(computer.car_has_any_defect).to eq true
+  end
+
+  it "should know if the car has any defect when it has no defect" do
+    seat = Seat.new
+    seat.set_defect NoDefect.new
+    car.parts << seat
+
+    expect(computer.car_has_any_defect).to eq false
+  end
+
 end
