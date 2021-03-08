d18f23dcddf7be844a0d479fcd23e04e52973efb
Matias Kamien <mkamien@medallia.com>
2021-03-08 12:03:23 -0300

    Add OrderGenerator to generate order

---
+++ app/services/order_generator.rb
@@ -0,0 +1,17 @@
+class OrderGenerator
+
+  def initialize warehouse
+    @warehouse = warehouse
+  end
+
+  def generate_order model
+    if (@warehouse.store_stock_by_model model) > 0
+      car = @warehouse.get_car_from_store model
+      order = Order.create! car_id:car.id
+      @warehouse.decrement_store car
+      return order
+    end
+    NoOrder.new
+  end
+
+end
---
+++ spec/services/order_generator_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe OrderGenerator, type: :model do
+
+  let(:warehouse) { double("Warehouse") }
+  let(:order_generator) { OrderGenerator.new warehouse }
+
+  it "creates a valid order when there is stock" do
+    mock_warehouse_with_stock
+    order = order_generator.generate_order "Corsa"
+    expect(order).to be_a(Order)
+    expect(order.car_id).to be 1
+  end
+
+  it "creates an invalid order when there is no stock" do
+    mock_warehouse_without_stock
+    order = order_generator.generate_order "Corsa"
+    expect(order).to be_a(NoOrder)
+  end
+
+  def mock_warehouse_with_stock
+    car = double("car", :id => 1)
+    allow(warehouse).to receive(:store_stock_by_model) { 1 }
+    allow(warehouse).to receive(:get_car_from_store) { car }
+    expect(warehouse).to receive(:decrement_store) { car }
+  end
+
+  def mock_warehouse_without_stock
+    allow(warehouse).to receive(:store_stock_by_model) { 0 }
+  end
+
+end
