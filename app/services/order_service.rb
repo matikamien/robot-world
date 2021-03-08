2d498d565670e558be899866e09e4f04b19049ad
Matias Kamien <mkamien@medallia.com>
2021-03-08 20:02:08 -0300

    Add get orders from today method to OrderService

--- app/services/order_service.rb
+++ app/services/order_service.rb
@@ -4,4 +4,12 @@ class OrderService
     Order.find_by(id:order_id)
   end
 
+  def get_orders_from_today
+    Order.all.select do |order|
+      order.created_at.day == Date.today.day &&
+        order.created_at.month == Date.today.month &&
+        order.created_at.year == Date.today.year
+    end
+  end
+
 end
