0c10b452bb791aceea3c29bb85a1e53927b97585
Matias Kamien <mkamien@medallia.com>
2021-03-09 18:32:12 -0300

    Add documentation to stock service methods

--- app/services/stock_service.rb
+++ app/services/stock_service.rb
@@ -8,6 +8,12 @@ class StockService
     @warehouse = Warehouse.new
   end
 
+  ##
+  # Check if there is stock in the warehouse for the received car model.
+  #
+  # First, it goes to check stock to the store stock. If there is no stock there,
+  # it goes to the factory stock checking that the car has no defects.
+
   def has_stock model
     store_stock = @warehouse.store_stock_by_model(model)
     store_stock > 0 ? true : check_stock_in_factory(model)
@@ -24,7 +30,13 @@ class StockService
     car_without_defect.nil? ? false : true
   end
 
-  # Return the requested car and decrement the stock.
+  # Return the requested car from the warehouse and also decrement the stock (mark the car as sold).
+  #
+  # First, it goes to get the car from the store. If the car is not there,
+  # it goes to the factory checking that the car has no defects.
+  #
+  # This method should be called after confirming there is stock available.
+
   def get_car_from_warehouse model
     car = @warehouse.get_car_from_store(model)
     !car.nil? ? sell_car(car) : get_car_from_factory(model)
