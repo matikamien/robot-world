09180bb114c49480d22a7fb7d3c87e05ca767b84
Matias Kamien <mkamien@medallia.com>
2021-03-09 19:55:46 -0300

    Update schedule file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-set :output, "log/cron.log"
+set :output, { error: 'log/error.log', standard: 'log/cron.log' }
 set :environment, "development"
 
-every 1.minute do
+every 1.minutes do
   runner "RobotBuilder.create_ten_cars"
 end
 
@@ -9,10 +9,10 @@ every 1.day, at: '11:59 pm' do
   runner "RobotBuilder.remove_incomplete_cars"
 end
 
-every 30.minute do
+every 30.minutes do
   runner "GuardRobotService.transfer_stock_and_notify_defects"
 end
 
-every 30.minute do
+every 30.minutes do
   runner "RobotBuyerService.buy_cars"
 end
